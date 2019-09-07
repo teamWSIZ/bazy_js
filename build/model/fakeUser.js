ce7978b074aac1e0d085b130cf96c5c9df5b5e6c
Piotr Piekielko <piotrpiekielko17@gmail.com>
2019-09-07 11:04:27 +0200

    batch insert

---
+++ build/model/fakeUser.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class FakeUser {
+    constructor(firstName, lastName, city, address, image) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.address = address;
+        this.image = image;
+    }
+}
+exports.FakeUser = FakeUser;
